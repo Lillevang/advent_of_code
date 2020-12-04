3c1a1b0c759d56a0318f67bdcada2b526ae00eb0
netcompany-jls <JLS@netcompany.com>
2020-12-04 23:35:38 +0100

    dec2-2019: did part 2

--- 2019/2/opcode_runner.py
+++ 2019/2/opcode_runner.py
@@ -1,32 +1,34 @@
 from typing import List
+import copy
 
 def read_input(path='input') -> List[int]:
     with open(path, 'r') as file:
         return [int(_.strip()) for _ in file.readline().split(',')]
 
 
-def add(x: int, y: int) -> int:
-    return x + y
-
-
-def mult(x: int, y: int) -> int:
-    return x * y
-
-
-def process_opcodes(data):
-    data[1] = 12
-    data[2] = 2
+def process_opcodes(data, noun=12, verb=2) -> int:
+    data[1] = noun
+    data[2] = verb
     for i in range(0, len(data), 4):
         if data[i] == 1:
-            data[data[i+3]] = add(data[data[i + 1]], data[data[i + 2]])
+            data[data[i+3]] = data[data[i + 1]] + data[data[i + 2]]
         elif data[i] == 2:
-            data[data[i+3]] = mult(data[data[i + 1]], data[data[i + 2]])
+            data[data[i+3]] = data[data[i + 1]] * data[data[i + 2]]
         elif data[i] == 99:
             break
     return data[0]
 
 
+def find_noun_and_verb(data: List[int]) -> int:
+    for i in range(100):
+        for j in range(100):
+            if process_opcodes(copy.deepcopy(data), i, j) == 19690720:
+                return 100 * i + j
+    return None
+
+
 if __name__ == "__main__":
     data = read_input()
-    print(process_opcodes(data))
+    print(process_opcodes(copy.deepcopy(data)))
+    print(find_noun_and_verb(data))
     
