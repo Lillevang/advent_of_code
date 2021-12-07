3e4cb79b832a586779a1bb2ae57dd8216f9534f2
Lillevang <jeppe.lillevang@gmail.com>
2021-12-07 21:38:31 +0100

    wip

--- 2021/05/solution.py
+++ 2021/05/solution.py
@@ -1,4 +1,4 @@
-from typing import List, Set
+from typing import List, Dict
 import sys
 
 
@@ -15,25 +15,23 @@ class Line:
     def __init__(self, ends: List[Point]) -> None:
         self.e1 = ends[0]
         self.e2 = ends[1]
-        self.is_horizontal_or_vertical = self.e1.x == self.e2.x or self.e1.y == self.e2.y
+        self.is_horizontal = self.e1.x == self.e2.x
+        self.is_vertical = self.e1.y == self.e2.y
+        self.is_horizontal_or_vertical = self.is_horizontal or self.is_vertical
+        self.points_on_line = self.find_points_on_line()
 
 
-class GeothermicVentMap:
-
-    def __init__(self, lines: List[Line]) -> None:
-        self.points = []
-        self.lines = lines
-        self.add_points()
-        self.process_data(lines)
-
-    def process_data(self, lines: List[Line]) -> None:
-        for line in lines:
-            pass
-
-    def add_points(self):
-        for i in range(1000):
-            for j in range(1000):
-                self.points.append(Point(i,j))
+    def find_points_on_line(self) -> List[str]: # Only horizontal or vertical lines
+        points_on_line = []
+        if self.is_vertical:
+            direction = 1 if self.e1.x < self.e2.x else -1
+            for i in range(self.e1.x, self.e2.x + direction, direction):
+                points_on_line.append(f'{i},{self.e1.y}')
+        elif self.is_horizontal:
+            direction = 1 if self.e1.y < self.e2.y else -1
+            for i in range(self.e1.y, self.e2.y + direction, direction):
+                points_on_line.append(f'{self.e1.x},{i}')
+        return points_on_line
 
 
 def read_input() -> List[Line]:
@@ -47,8 +45,20 @@ def read_input() -> List[Line]:
             lines.append(Line(line_ends))
         return lines
 
+def get_all_points() -> Dict[str, int]:
+    points = {}
+    for i in range(1000):
+        for j in range(1000):
+            points[f'{i},{j}'] = 0
+    return points
+
+
 def part_one(lines: List[Line]):
-    geothermic_vent_map = GeothermicVentMap([_ for _ in lines if _.is_horizontal_or_vertical])
+    points = get_all_points()
+    for line in lines:
+        for point in line.points_on_line:
+            points[point] += 1
+    return sum([_ for _ in points.values() if _ >= 2])
 
 
 def part_two(lines):
