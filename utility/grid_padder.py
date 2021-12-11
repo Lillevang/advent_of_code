c0ecd27ce8ed21db592ac7d753e4996b3fedfe81
Lillevang <jeppe.lillevang@gmail.com>
2021-12-11 02:17:42 +0100

    solved the days I was behind - even did som pl/sql

--- utility/grid_padder.py
+++ utility/grid_padder.py
@@ -1,13 +1,14 @@
-from typing import List
+from typing import List, Generic, TypeVar
 
-class GridPadder:
+T = TypeVar("T")
+class GridPadder(Generic[T]):
 
-    def __init__(self, base_grid: List[List[str]], pad_char: str) -> None:
+    def __init__(self, base_grid: List[List[T]], pad_char: T) -> None:
         self.base_grid = base_grid
         self.padded_grid = self.pad_grid(base_grid, pad_char)
 
 
-    def pad_grid(self, base_grid: List[List[str]], pad_char: str) -> List[str]:
+    def pad_grid(self, base_grid: List[List[T]], pad_char: T) -> List[List[T]]:
         padded_grid = []
         top_and_bot_row = [pad_char] * (len(base_grid[0]) + 2)
         padded_grid.append(top_and_bot_row)
